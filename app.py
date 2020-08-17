b081b9ad8e097918683e951b672bd7927226fc7a
haruzong1 <41902380+haruzong1@users.noreply.github.com>
2020-08-17 22:22:34 +0900

    17일 프론트까지만

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
-
+from flask.cli import load_dotenv
+from pymongo import MongoClient
+from pytz import timezone
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
 
-
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/info', methods=['POST'])
+def resume_info():
 
+    writer_recieve = request.form['writer_give']
+    document = {
+        'writer': writer_recieve,
+            }
+    db.review.insert_one(document)
+    return jsonify({'result': 'success', 'msg': '리뷰가 성공적으로 작성되었습니다.'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
